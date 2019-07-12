7c51cf1ee67bcdeb831c555e779b667aa4078c16
stevensa2 <alyssa.stevens@pobox.com>
2019-07-12 14:08:41 -0700

    read data into program

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,15 +1,11 @@
-dir.create("specdata")
-setwd("specdata")
-unzip(specdata.zip)
-read.csv(/Downloads/specdata.zip)
-#I really don't know how to code into reading a zipped file and then unzipping it or vice versa
+for (i in 1:332) {read.csv(paste("Desktop/CourseraWork/RProgramming/Week2/specdata/", sprintf("%0*i", 3, i), ".csv", sep=''))}
 
 #make polutantmean function that calculates the mean of a pollutant across a list of monitors. the function take three arguments: directory, pollutant, and id. given a vector monitor ID numbers, pollutantmean reads that monitors particulate matter data in the directory specified in the directory argument and returns the mean of the pollutant across all the monitors, ignoring any missing values coded as NA.#
 
-pollutantmean <- function(directory, pollutant, id) {
-	directory <- 
-	pollutant <-
-	id <- 
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+	## directory <- 
+	## pollutant <-
+	## id <- 
 }
 	
 
