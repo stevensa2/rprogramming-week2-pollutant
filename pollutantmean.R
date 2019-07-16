67ed189bbea97a45d296b41e9b4f26993cff979f
stevensa2 <alyssa.stevens@pobox.com>
2019-07-16 12:37:03 -0700

    third update, took out second loop and added information for every monitor within every id. still not reading data or getting results. should return a vector from means() but nothing occurs - unsure of where error lies

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,5 +1,3 @@
-for (i in 1:332) {read.csv(paste("Desktop/CourseraWork/RProgramming/Week2/specdata/", sprintf("%0*i", 3, i), ".csv", sep=''))}
-
 #make polutantmean function that calculates the mean of a pollutant across a list of monitors. the function take three arguments: directory, pollutant, and id. given a vector monitor ID numbers, pollutantmean reads that monitors particulate matter data in the directory specified in the directory argument and returns the mean of the pollutant across all the monitors, ignoring any missing values coded as NA.#
 
 	## 'directory' is a character vector of length 1 indicating the location of the csv files
@@ -7,20 +5,16 @@ for (i in 1:332) {read.csv(paste("Desktop/CourseraWork/RProgramming/Week2/specda
 	## 'id' is an integer vector indicating the monitor ID numbers to be used
 	## return the mean of the pollutant across all monitors list in the 'id' vector (ignoring NA values) and do not round the result
 
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-for (monitor in id){
-	path <- paste(get(wd),"/", directory, "/", sprintf("%0*i", 3, i), ".csv", sep='')
+	means <- c()
+	for (monitor in id){
+	path <- paste(get(wd)), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep="")
+	data <- read.csv(path)
+	used_data <- data[pollutant]
+	means <- c(means, used_data[!is.na(used_data)])
+}
 }
 
-
-
-	
-	directory <- 
-	
-	pollutanttotal <- sum(pollutant)
-	pollutantlength <- length(pollutant)
-	pollutanttotal/pollutantlength
-
-}
 	
 
