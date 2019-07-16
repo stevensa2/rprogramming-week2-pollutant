ae85a096367e5a922d804f65df5f95295c7edc7b
stevensa2 <alyssa.stevens@pobox.com>
2019-07-16 13:02:02 -0700

    first changes on second program needed, doesn't run, unsure about how to fix (probably has something to do with first program not running either)

--- complete.R
+++ complete.R
@@ -1,10 +1,25 @@
-#write a function that reads a directory fully of files and reports the number of completely observed cases in each data file. the function should return a data fram where the first column is the name of the file and the second column is the number of complete cases
+##write a function that reads a directory fully of files and reports the number of completely observed cases in each data file. the function should return a data fram where the first column is the name of the file and the second column is the number of complete cases
 
-#use same import code from pollutantmean.R
-#
+## 'director' is a character vector of length 1 indicating
+## the location of the CSV files
 
-complete <- function(directory, id){
-	directory <- #this can probably come from the code of the first function#
-	id <- #same here#
-	#we need to read the data and make a matrix of results where the id and number of complete cases are the columns and the rows are the directories where this info is coming from
+## 'id' is an integer vector indicating the monitor ID numbers
+## to be used
+
+## Return a data frame of the from:
+## id nobs
+## 1  117
+## 2  1041
+## ...
+## where 'id' is the monitor ID number and 'nobs' is the
+## number of complete cases
+
+complete <- function(directory, id = 1:332){
+  end_results <- data.frame(id = numeric(0), nobs = numeric(0))
+  path <- paste(get(wd)), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep='')
+  data <- read.csv(path)
+  data_used <- data[(!is.na(data$sulfate)), ]
+  data_used <- data[(!is.na(data$nitrate)), ]
+  nobs <- nrows(data_used)
+  end_results <- rbind(end_results, data.fram(id = monitor, nobs = nobs))
 }
