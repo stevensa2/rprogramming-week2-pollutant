e582548c28af179a2e26d41f09459477f4b72a7a
stevensa2 <alyssa.stevens@pobox.com>
2019-07-16 13:17:16 -0700

    third function needed, still not running, have syntax errors that I am unsure of how to fix... looked up inputs for functions but am not sure if they are correct especially subsets of data_used and cor

--- corr.R
+++ corr.R
@@ -2,7 +2,29 @@
 #will need to use the code from the first function to read in the information, and the code from the second function to look at complete cases#
 
 corr <- function(directory, threshold){
-	directory <-
-	threshold <- #numeric vector of length 1 indicating the number of completely observed observations required to compute the correlation betweeen nitrate and sulfate, the default is 0#
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the 
+  ## number of completely observed observations (on all
+  ## variables) requi?red to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  ## NOTE: Do not round the result!
+corr <- function(directory, threshold){
+  if(nrow(complete_cases > 0)){
+    for(monitor in id){
+      path <- path <- paste(get(wd)), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep='')
+      data <- read.csv(path)
+      data_used <- data[(!is.na(data$sulfate)), ]
+      data_used <- data[(!is.na(data$nitrate)), ]
+      sulfate <- data_used["sulfate"]
+      nitrate <- data_used["nitrate"]
+      correlation <- cor(sulfate, nitrate)
+    }
+  }
+  correlation()
+}
 	
 }
