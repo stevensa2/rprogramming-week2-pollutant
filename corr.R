97082530e250cc9c4defc17baa55d699bcf0296c
stevensa2 <alyssa.stevens@pobox.com>
2019-07-21 14:53:41 -0700

    Syntax errors fixed within all three programs, centered around brackets and variable assignments

--- corr.R
+++ corr.R
@@ -1,20 +1,20 @@
 #write a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases is greater than the threshold. the function should return a vector of correlations for the monitors that meet the threshold requirement.
 #will need to use the code from the first function to read in the information, and the code from the second function to look at complete cases#
 
-corr <- function(directory, threshold){
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
+## 'directory' is a character vector of length 1 indicating
+## the location of the CSV files
   
-  ## 'threshold' is a numeric vector of length 1 indicating the 
-  ## number of completely observed observations (on all
-  ## variables) requi?red to compute the correlation between
-  ## nitrate and sulfate; the default is 0
+## 'threshold' is a numeric vector of length 1 indicating the 
+## number of completely observed observations (on all
+## variables) requi?red to compute the correlation between
+## nitrate and sulfate; the default is 0
   
-  ## Return a numeric vector of correlations
-  ## NOTE: Do not round the result!
-corr <- function(directory, threshold){
-  if(nrow(complete_cases > 0)){
-    for(monitor in id){
+## Return a numeric vector of correlations
+## NOTE: Do not round the result!
+
+corr <- function(directory, threshold) {
+  if(nrow(complete_cases > 0)) {
+    for(monitor in id) {
       path <- path <- paste(get(wd)), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep='')
       data <- read.csv(path)
       data_used <- data[(!is.na(data$sulfate)), ]
@@ -27,4 +27,3 @@ corr <- function(directory, threshold){
   correlation()
 }
 	
-}
--- pollutantmean.R
+++ pollutantmean.R
@@ -1,19 +1,19 @@
 #make polutantmean function that calculates the mean of a pollutant across a list of monitors. the function take three arguments: directory, pollutant, and id. given a vector monitor ID numbers, pollutantmean reads that monitors particulate matter data in the directory specified in the directory argument and returns the mean of the pollutant across all the monitors, ignoring any missing values coded as NA.#
 
-	## 'directory' is a character vector of length 1 indicating the location of the csv files
-	## 'pollutant' is a character vector of length 1 indicating the name of the pollutant for which we will calculate the mean; either "sulfate" or "nitrate"
-	## 'id' is an integer vector indicating the monitor ID numbers to be used
-	## return the mean of the pollutant across all monitors list in the 'id' vector (ignoring NA values) and do not round the result
+## 'directory' is a character vector of length 1 indicating the location of the csv files
+## 'pollutant' is a character vector of length 1 indicating the name of the pollutant for which we will calculate the mean; either "sulfate" or "nitrate"
+## 'id' is an integer vector indicating the monitor ID numbers to be used
+## return the mean of the pollutant across all monitors list in the 'id' vector (ignoring NA values) and do not round the result
 
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
 	means <- c()
-	for (monitor in id){
+	for (monitor in id) {
 	path <- paste(get(wd)), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep="")
 	data <- read.csv(path)
 	used_data <- data[pollutant]
 	means <- c(means, used_data[!is.na(used_data)])
-}
+  }
 }
 
 	
